f91787fe20e5fa24b783b027c43eb2bc4c3172f2
Atharv0825 <atharvbabar044@gmail.com>
2026-01-19 14:36:27 +0530

    first commit

---
+++ .history/Main_20260119143302.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is Java program");
+        System.out.println(new Date);
+        System.out.println("Output from java program");
+
+    }
+}
